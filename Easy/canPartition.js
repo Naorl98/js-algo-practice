0701074761f9bbe044adba623db1e74230c38340
Naorl98 <Naorman12@gmail.com>
2025-07-09 13:00:57 +0300

    3 first leetcode

--- Easy/canPartition.js
+++ Easy/canPartition.js
@@ -1,20 +1,26 @@
-/*
-Write a function that returns true if you can partition an array into one element and the rest, such that this element is equal to the product of all other elements excluding itself.
+function canPartition(arr) {
+  let totalProduct = 1
+  let zeroCount = 0
+  if (arr.length < 2) return false
+  for (let num of arr) {
+    if (num === 0) {
+      zeroCount++
+    }
+    else{
+    totalProduct *= num
+    }
+  }
 
-Examples
-canPartition([2, 8, 4, 1]) ➞ true
-// 8 = 2 x 4 x 1
+  for (let num of arr) {
+    if (num === 0) {
+      if (zeroCount > 1) return true
+      else return false
+    } else {
+      if (totalProduct / num === num) return true
+    }
+  }
 
-canPartition([-1, -10, 1, -2, 20]) ➞ false
-
-canPartition([-1, -20, 5, -1, -2, 2]) ➞ true
-Notes
-The array may contain duplicates.
-Multiple solutions can exist, any solution is sufficient to return true.
-*/
-
-function canPartition( /*args*/ ) {
-  //your code
+  return false
 }
 
-exports.solution = canPartition;
+exports.solution = canPartition
